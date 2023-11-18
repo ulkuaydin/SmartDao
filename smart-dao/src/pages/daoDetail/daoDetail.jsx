167a740b9ce4f5c650841f658c9da4ed5d2dac50
Cagatay Yildirim <yildirimcagatay1987@hotmail.com>
2023-11-19 02:50:36 +0300

    calisio

--- smart-dao/src/pages/daoDetail/daoDetail.jsx
+++ smart-dao/src/pages/daoDetail/daoDetail.jsx
@@ -1,9 +1,42 @@
 import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import Web3 from "web3";
+import { useState } from "react";
+import SmartDaoService from "../../utils/smartDaoService";
 
 const DaoDetail = () => {
   const { id } = useParams();
   useEffect(() => {}, []);
+  const [account, setAccounts] = useState(null);
+
+  useEffect(() => {
+    // Create an instance of Web3
+    const web3 = new Web3(window.ethereum);
+
+    // Fetch the accounts
+    web3.eth
+      .getAccounts()
+      .then((accounts) => {
+        let newService = new SmartDaoService(
+          "0xb00A7CD04b0d005702aFC4f5ccB3671E0F5bF512", //TODO get this from app consntan or use service singleton
+          web3,
+          accounts[0]
+        );
+
+        newService.getDAOs(id).then((dao) => {
+          // Update the state with the fetched accounts
+          if (dao ==null){
+            return;
+          }
+          setAccounts(dao);
+        });
+      })
+      .catch((error) => {
+        // Handle errors, such as if the user has not enabled MetaMask
+        console.error("Error fetching accounts:", error);
+      });
+  }, []); // The empty array as the second argument ensures this runs once on mount
+console.log('connected account',account);
   return (
     <div className="container">
       <div className="row g-5">
@@ -14,13 +47,13 @@ const DaoDetail = () => {
           >
             <div className="discover-dao-item d-flex flex-column align-items-center">
               <div className="logo d-flex align-items-center justify-content-center">
-                <span>DN</span>
+                <span>{account?.daoName.slice(0,2)}</span>
               </div>
               <h6
                 className="text-center mt-2"
                 style={{ fontWeight: "700", fontSize: "25px" }}
               >
-                Dao Name
+                { account?.daoName }
               </h6>
             </div>
 
@@ -89,9 +122,7 @@ const DaoDetail = () => {
                           </div>
                         </div>
                         <div className="col-lg-9">
-                          <div className="progress-choice position-relative">
-                            
-                          </div>
+                          <div className="progress-choice position-relative"></div>
                         </div>
                       </div>
                     </div>
